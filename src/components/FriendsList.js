963f96a546feeb4af908a3f1d2c0bd86b90d5778
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-09-25 07:31:35 +0200

    rendered friendslist

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Friend from "./Friend.js";
 
 const FriendsList = ({ friends }) => {
   return (
     <ul>
       {friends.map((friend) => (
-        <li>{friend.name}</li>
+        <Friend friend={friend} key={friend.id} />
       ))}
     </ul>
   );
